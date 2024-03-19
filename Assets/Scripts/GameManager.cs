92e5121ba423b3ecf4b137462061b3d01c72ccaf
LPowell2022 <LPowell2022@elam.co.uk>
2024-03-19 11:53:24 +0000

    Prototype nearing completion

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player 1")]
+    public GameObject player1Paddle;
+    public GameObject player1Goal;
+
+    [Header("Player 2")]
+    public GameObject player2Paddle;
+    public GameObject player2Goal;
+
+    [Header("Score UI")]
+    public GameObject Player1Text;
+    public GameObject Player2Text;
+}
