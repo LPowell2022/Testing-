3d3f31d0007c50e4bffefbd2ac7c1841e88d7850
LPowell2022 <LPowell2022@elam.co.uk>
2024-03-25 10:59:03 +0000

    Ya

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+
     public bool isPlayer1Goal;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag ("Ball"))
+        if(collision.gameObject.CompareTag("Ball"))
         {
-            if (isPlayer1Goal)
+          if (isPlayer1Goal)
             {
                 Debug.Log("Player 2 Scored...");
                 GameObject.Find("GameManager").GetComponent<GameManager>().Player2Scored();
             }
-            else
+          else
+            {
                 Debug.Log("Player 1 Scored...");
                 GameObject.Find("GameManager").GetComponent<GameManager>().Player1Scored();
+                   
+            }
         }
     }
 
