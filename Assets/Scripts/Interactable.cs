726347b6e915f97caac4b5a1f34d8e350a60d2b9
LPowell2022 <LPowell2022@elam.co.uk>
2024-04-16 11:52:00 +0100

    next update guys

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        OnTriggerEnter(GetKeyDown)
+        
+    }
+}
--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -17,8 +17,17 @@ public class TitleScreen : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void CampaignMode()
+    {
+        SceneManager.LoadScene(2);
+    }
     public void LoadGame()
     {
         SceneManager.LoadScene(3);
+    }
+
+    public void GameModes()
+    {
+        SceneManager.LoadScene(4);
     }
 }
