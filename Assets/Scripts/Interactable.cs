670745c0620a8311b2b45fd466e1c11686311964
LPowell2022 <LPowell2022@elam.co.uk>
2024-04-18 10:46:53 +0100

    vibes

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Interactable : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
-    {
-        OnTriggerEnter(GetKeyDown)
-        
-    }
+        void OnTriggerEnter(Collider other)
+        {
+                {
+                    SceneManager.LoadScene(1);
+                }
+        }
 }
